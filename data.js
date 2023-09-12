d6700539663047479358388573a58057a6b73189
bhabani <bhabani@hutechsolutions.com>
2023-09-12 15:14:43 +0530

    separate a static data into file, import in index

---
+++ data.js
@@ -0,0 +1,5 @@
+const data = [
+  { name: "Bhabani", age: 25, email: "bhabani@gmail.com" },
+  { name: "Anjali", age: 25, email: "anjali@gmail.com" },
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const http = require("http");
+const data = require("./data");
 // Create a server with header and api body and port 5000 getting a static json code
 http
   .createServer((req, resp) => {
     resp.writeHead(200, {
       "Content-Type": "application/json",
     });
-    resp.write(
-      JSON.stringify({ name: "Bhabani", age: 25, email: "bhabani@gmail.com" })
-    );
+    resp.write(JSON.stringify(data));
     resp.end();
   })
   .listen(5000);
